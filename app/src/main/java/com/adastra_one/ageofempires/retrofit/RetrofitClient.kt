fea60e6716ee68490925057ca0957b596a7cd886
Ekene Mefor <ekenemefor@Ekenes-MacBook-Air.local>
2022-09-09 21:41:13 +0200

    Added dependencies required by project

---
+++ app/src/main/java/com/adastra_one/ageofempires/retrofit/RetrofitClient.kt
@@ -0,0 +1,41 @@
+package app.medrx.MedrxApp.retrofit
+
+import androidx.viewbinding.BuildConfig
+import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    const val MainServer = "http://medrx.indieconsortium.net/api/"
+        //"https://medrxadminpanel.azurewebsites.net/api/" //"http://medrx.indieconsortium.net/api/"
+
+
+    const val ImageMainServer = "";
+
+    val retrofitClient: Retrofit.Builder by lazy {
+
+        val levelType: Level
+        if (BuildConfig.BUILD_TYPE.contentEquals("debug"))
+            levelType = Level.BODY else levelType = Level.NONE
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(levelType)
+
+        val okhttpClient = OkHttpClient.Builder()
+        okhttpClient.addInterceptor(logging)
+
+        Retrofit.Builder()
+            .baseUrl(MainServer)
+            .client(okhttpClient.build())
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    val apiInterface: ApiInterface by lazy {
+        retrofitClient
+            .build()
+            .create(ApiInterface::class.java)
+    }
+}
